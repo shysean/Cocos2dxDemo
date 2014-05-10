5d58f78c1f00a43780001469b54b7852224f3074
shysean <shihongyang0218@gmail.com>
2014-05-10 19:06:13 +0800

    2014-05-10 add create test file script

---
+++ MyCocosDemo/script/createTest.py
@@ -0,0 +1,60 @@
+# Filename: createTest.py
+
+import sys
+import time
+import os
+
+header_template_file = './test_template_header'
+source_template_file = './test_template_source'
+
+out_path = '../Classes/TestCase/'
+# get param
+if len(sys.argv) < 2:
+    print "Please input class name."
+    print sys.argv[0]
+    sys.exit(0)
+
+className = sys.argv[1]
+
+# get create date
+createDate = time.strftime('%m/%d/%y',time.localtime(time.time()))
+
+# create Header File
+headerTemplateFile = file(header_template_file,'r')
+headerFile = out_path + className + '.h'
+
+if os.path.exists(headerFile):
+    print headerFile + ' has exists!'
+else:
+    headerOutData = file(headerFile, 'w')
+
+    for line in headerTemplateFile:
+        outLine = line.replace('#CLASSNAME#', className).replace('#TIME#', createDate)
+        headerOutData.write(outLine)
+    headerOutData.close()
+
+    print headerFile + ' has created!'
+
+headerTemplateFile.close()
+
+
+# create Source File
+sourceTemplateFile = file(source_template_file,'r')
+sourceFile = out_path + className + '.cpp'
+
+if os.path.exists(sourceFile):
+    print sourceFile + ' has exists!'
+else:
+    sourceOutData = file(sourceFile, 'w')
+
+    for line in sourceTemplateFile:
+        outLine = line.replace('#CLASSNAME#', className).replace('#TIME#', createDate)
+        sourceOutData.write(outLine)
+    sourceOutData.close()
+
+    print sourceFile + ' has created!'
+
+sourceTemplateFile.close()
+
+
+sys.exit(0)
